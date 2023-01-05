97fb130bbfa6694d9e7f2d9d3849e2bfd201a1d3
Natalie Reilly <102992414+AgateHunter@users.noreply.github.com>
2023-01-04 17:55:22 -0800

    Part 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        int i = 4;
+        double d = 4.0;
+        string s = "HackerRank ";
+
+
+        // Declare second integer, double, and String variables.
+        int ii = 8;
+        double dd = 5.3;
+        string ss = "provides me a space to practice and build my skills";
+
+        // Read and save an integer, double, and String to your variables.
+        Console.ReadLine();
+        // Print the sum of both integer variables on a new line.
+        Console.WriteLine(i + ii);
+
+        // Print the sum of the double variables on a new line.
+        Console.WriteLine(d + dd);
+
+        // Concatenate and print the String variables on a new line
+
+        Console.WriteLine(s + ss);
+        // The 's' variable above should be printed first.
+
+    }
+}
